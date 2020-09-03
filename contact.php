ee91fc22aacae9b4c21d2a959cc821c2c051c781
YASHIKA KALRA <yashika25kalra@gmail.com>
2020-09-03 17:39:38 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
+
+    $email_from = "Yashika Kalra";
+    $email_subject = "New Mail From Contact Form";
+    $email_body =   "name: $name.\n".
+                    "email: $email.\n".
+                    "phone: $phone.\n".
+                    "message: $message. \n";
+
+    $to = 'y.colkalra100@gmail.com';
+    $headers = "FROM: $email_from \r\n";
+    $headers = "REPLY-TO: $email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("location: index.html");
+
+?>
